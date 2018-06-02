88d8c89af7df4b455ad2f9b531ec3b2ade9cc988
artsaldel <artsaldel@hotmail.com>
2018-06-02 09:50:02 -0600

    Cambios en el documento de diseño

--- prueba.c
+++ prueba.c
@@ -1,43 +1,17 @@
 #include <stdio.h>
 
-int N = 25;
-int BUFFER_MAX_SIZE = 25;
-
-void IcNeighbors(int VD[BUFFER_MAX_SIZE],int IC[BUFFER_MAX_SIZE],int CONN[BUFFER_MAX_SIZE][BUFFER_MAX_SIZE])
-{
-	int V = 0;///////////////////////
-	int i,j;
-	int Facc = 0;///////////////////////
-	int Vacc = 0;///////////////////////
-	int f = 0;///////////////////////
-	for(i=0;i<N;i++)
-	{
-		for(j=0;j<N;j++)
-		{
-			V = VD[i]-VD[j];
-			f = V*34;
-			Facc += f*CONN[i][j];
-			Vacc += V*CONN[i][j];
-		}
-		IC[i] = 20*Facc + 80*Vacc ;
-	}
-}
-
 void main()
-{
-	/*float num = 0;
-	for (int  i = 0; i < 25; i++)
+{	
+	int N = 10000;
+	float contador1 = 0.0;
+	float contador2 = 0.0;
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < 25; j++)
+		contador1 = contador1 + 1.0;
+		for (int j = 0; j < N; j++)
 		{
-			num = num + 1.0;
+			contador2 = contador2 + 1.0;
 		}
-	}*/
-
-	
-	int VD[N];
-	int IC[N];
-	int CONN[N][N];
-	IcNeighbors(VD,IC,CONN);
+	}
 }
 
